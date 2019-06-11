c4b2d5a86b8c81fe146fd95b11dc2d3c6f616911
yee.wang <wyy.xb@qq.com>
2019-06-11 19:53:28 +0800

    feat: add viewSchema unit test

--- src/models/ViewSchema.ts
+++ src/models/ViewSchema.ts
@@ -2,11 +2,12 @@ import { IView } from '../interface';
 import * as CSS from 'csstype';
 import { Vmo } from '@vmojs/base';
 import { Field } from '@vmojs/base/bundle';
-import { generateUid, isArray, isEficyView } from '../utils';
-import { action, computed, observable } from 'mobx';
-import { mapObjectDeep } from '../utils';
+import { generateUid, isArray, isEficyView, mapObjectDeep } from '../utils';
+import { action, computed, extendObservable, observable } from 'mobx';
 
 export default class ViewSchema extends Vmo implements IView {
+  public static readonly solidField = ['#', '#view', '#children', '#restProps'];
+
   @Field
   public '#view': string;
   @Field
@@ -27,13 +28,13 @@ export default class ViewSchema extends Vmo implements IView {
   @observable
   public style: CSS.Properties;
 
-  @action
+  @action.bound
   protected load(data: IView): this {
     if (!data['#']) {
       data['#'] = generateUid();
     }
 
-    this.update(data);
+    this.update(data, true);
 
     return this;
   }
@@ -65,7 +66,7 @@ export default class ViewSchema extends Vmo implements IView {
   }
 
   @action.bound
-  public update(data: IView): this {
+  public update(data: IView, isInit = false): this {
     if (!this['#restProps']) {
       this['#restProps'] = {};
     }
@@ -73,18 +74,24 @@ export default class ViewSchema extends Vmo implements IView {
     data = this.transformViewSchema(data);
 
     Object.keys(data).forEach(key => {
+      if (!isInit && ViewSchema.solidField.includes(key)) {
+        if (data[key] === this[key]) {
+          return;
+        }
+        throw new Error(`Can not update static property ${ViewSchema.solidField.join(',')}`);
+      }
+
       // @ts-ignore
       if (this.__proto__.hasOwnProperty(key)) {
         this[key] = data[key];
       } else {
         this['#restProps'][key] = data[key];
         if (!this.hasOwnProperty(key)) {
-          Object.defineProperty(this, key, {
-            enumerable: true,
-            get() {
+          extendObservable(this, {
+            get [key]() {
               return this['#restProps'][key];
             },
-            set(val) {
+            set [key](val) {
               this['#restProps'][key] = val;
             },
           });
