f9f1ec1f3ee2aa5be938d8344fb5f3c8b8916017
yee.wang <wyy.xb@qq.com>
2019-06-11 14:48:53 +0800

    feat: independent the Hook package

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,4 +4,3 @@ export { default as mergeClassName } from './mergeClassName';
 export { default as isEficyView } from './isEficyView';
 export { default as mapObjectDeep } from './mapObjectDeep';
 export * from './common';
-export * from './HookDecorator';
