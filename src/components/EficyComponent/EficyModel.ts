d896a63dc8415495790efd53be875cbe284137e9
xiaobe <wyy.xb@qq.com>
2019-06-22 15:36:02 +0800

    feat: add parent children eficy event

--- src/components/EficyComponent/EficyModel.ts
+++ src/components/EficyComponent/EficyModel.ts
@@ -1,6 +1,8 @@
 import { ViewSchema } from '../../models';
 import { IPlugin, IView } from '../../interface';
 import { Field } from '@vmojs/base';
+import EficyController from '../../core/Controller';
+import { action, computed, observable } from 'mobx';
 
 export default class EficyModel extends ViewSchema {
   @Field
@@ -9,4 +11,24 @@ export default class EficyModel extends ViewSchema {
   public views: IView[];
   @Field
   public plugins?: IPlugin[];
+  @Field
+  public events?: any;
+
+  @observable.ref
+  public '#controller'?: EficyController;
+
+  @computed
+  public get models() {
+    return this['#controller'] ? this['#controller'].models : {};
+  }
+
+  @action.bound
+  public bindController(controller: EficyController) {
+    this['#controller'] = controller;
+  }
+
+  @action.bound
+  public removeController(controller: EficyController) {
+    this['#controller'] = undefined;
+  }
 }
