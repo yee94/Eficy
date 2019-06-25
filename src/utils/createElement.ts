acf2c61c2dc302321c5dce254d315d907189f4d6
xiaobebe <wyy.xb@qq.com>
2019-06-25 12:02:15 +0800

    fix: fix creatElement

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -2,7 +2,7 @@ import { IEficySchema } from '../interface';
 import EficyController from '../index';
 import React from 'react';
 
-export default function(schema: IEficySchema, componentMap?: Record<string, React.Component>) {
+export default function(schema: IEficySchema, componentMap?: Record<string, React.Component | any>) {
   const controller = new EficyController(schema, componentMap);
 
   return controller.resolver();
