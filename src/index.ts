d9cb4190720f6222edd5bf903b11f03afc616776
yee.wang <wyy.xb@qq.com>
2019-06-26 16:52:58 +0800

    feat: add base plugin export

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export { install as installPlugin } from './plugins';
 export const Tools = _Tools;
 export const Models = _Models;
 export { default as render } from './utils/renderHelper';
+export { default as ViewSchema } from './models/ViewSchema';
 export { default as createElement } from './utils/createElement';
 export { default as EficyComponent } from './components/EficyComponent';
 export const controller = Controller;
