569320bf66553d93b285255a802448fa4fd38cba
xiaobebe <wyy.xb@qq.com>
2020-03-05 00:05:38 +0800

    feat: update

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -1,7 +1,7 @@
-import { IEficySchema } from '../interface';
+import { IEficySchema, IView } from '../interface';
 import React from 'react';
 import EficyComponent from '../components/EficyComponent';
 
-export default function(schema: IEficySchema, componentMap?: Record<string, React.Component | any>) {
+export default function(schema: IEficySchema | IView, componentMap?: Record<string, React.Component | any>) {
   return React.createElement(EficyComponent, { model: schema, componentMap });
 }
