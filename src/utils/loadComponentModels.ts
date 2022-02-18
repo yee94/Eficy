1d31206d0fff2104b09737f6ec202366e21830b6
Justinidlerz <zqc.sunny@gmail.com>
2022-02-18 11:10:46 +0800

    Fix types

--- src/utils/loadComponentModels.ts
+++ src/utils/loadComponentModels.ts
@@ -1,15 +1,16 @@
 import forEachDeep from './forEachDeep';
 import ViewNode from '../models/ViewNode';
+import EficyComponent from '../components/EficyComponent';
 
 export default function loadComponentModels(
   componentLibrary: Record<string, any>,
 ): Record<string, new (...args: any) => ViewNode> {
   const componentModelMap = {};
 
-  forEachDeep(componentLibrary, (objMaps) => {
-    Object.keys(objMaps).forEach((key) => {
-      const component = objMaps[key];
-      if (component?.EficyModel) {
+  forEachDeep(componentLibrary, objMaps => {
+    Object.keys(objMaps).forEach(key => {
+      const component = objMaps[key] as typeof EficyComponent;
+      if (component && component.EficyModel) {
         componentModelMap[key] = component.EficyModel;
       }
     });
